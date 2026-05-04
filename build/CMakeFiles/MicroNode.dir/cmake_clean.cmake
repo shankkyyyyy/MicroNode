a9fec35c499e19f32b99b5f5fd54ea69ce33c25e
shankkkyyyyy <ramanshikillu@gmail.com>
2026-05-04 22:55:31 +0530

    Inital Commit -2

---
+++ build/CMakeFiles/MicroNode.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MicroNode.dir/src/main.c.o"
+  "CMakeFiles/MicroNode.dir/src/main.c.o.d"
+  "MicroNode"
+  "MicroNode.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MicroNode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/MicroNode.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for MicroNode.
+# This may be replaced when dependencies are built.
