77816ae31131fbfb4c42dcc8bd7eb80a15def2b8
shankkkyyyyy <ramanshikillu@gmail.com>
2026-05-04 22:54:13 +0530

    Inital Commit

---
+++ src/main.c
@@ -0,0 +1,14 @@
+#ifndef NETWORKING_H
+    #define NETWORKING_H
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <error.h>
+
+int main()
+{
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
+
+#endif
